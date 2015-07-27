feee8c04654cc7a2790e6e14391ae9e22e76ae9b
matulik <matulik@o2.pl>
2015-07-27 15:48:08 +0200

    All required base model method added. Refreshing a GameBoard started.

--- robotsw/Field.swift
+++ robotsw/Field.swift
@@ -12,20 +12,26 @@ class Field {
     // Type of field
     // 0 - not set, 1 - player, 2 - enemy, 3 - crashed enemy, 4 - bonus?
     var fieldType : Int = 0
+    var x : Int = -1
+    var y : Int = -1
     
     init() {
         self.setFieldType(fieldType: 0)
     }
     
-    init(fieldType: Int){
+    init(x: Int, y: Int, fieldType: Int){
         if (self.setFieldType(fieldType: fieldType)){
             //println("Field initialized")
+        }
+        else if (x < 0 || y < 0) {
+            println("Bad x or y value")
         }
         else {
             self.setFieldType(fieldType: 0)
         }
     }
     
+    // FieldType g/s.
     // Return true if allright
     func setFieldType(#fieldType : Int) -> Bool {
         if (fieldType > 4 || fieldType < 0) {
@@ -36,8 +42,19 @@ class Field {
             return true
         }
     }
-    
+    //
     func getFieldType() -> Int {
         return self.fieldType
+    }
+    
+    // XY g/s
+    func setFieldXY(#x: Int, y: Int) {
+        if (x < 0 || y < 0) {
+            println("Bad x or y value")
+        }
+        else {
+            self.x = x
+            self.y = y
+        }
     }
 }
--- robotsw/Table.swift
+++ robotsw/Table.swift
@@ -11,12 +11,14 @@ import Foundation
 class Table {
     var contentArray = Array<Array<Field>>()
     var enemiesArray = Array<Field>()
-    var playerArray = Field()
+    var player = Field()
     var x : Int = 0
     var y : Int = 0
     
     var numberOfEnemies : Int = 0
     var numberOfTeleports : Int = 0
+    
+    
     
     init(x : Int, y : Int) {
         self.setContentArraySize(x: x, y: y)
@@ -24,14 +26,21 @@ class Table {
     
     // Array g/s
     func setContentArraySize(#x : Int, y : Int) {
+        self.clearContentArray()
+        self.clearEnemiesArray()
+        self.player = Field()
+        self.numberOfEnemies = 0
+        self.numberOfTeleports = 0
+        
         self.x = x
         self.y = y
+        
         for n in 0..<x {
-            self.contentArray.append(Array(count: y, repeatedValue: Field(fieldType: 0)))
+            self.contentArray.append(Array(count: y, repeatedValue: Field()))
         }
         for x in 0..<self.x {
             for y in 0..<self.y {
-                self.contentArray[x][y] = Field(fieldType: 0)
+                self.contentArray[x][y] = Field()
             }
         }
     }
@@ -71,6 +80,10 @@ class Table {
         return self.numberOfEnemies
     }
     
+    func clearEnemiesArray() {
+        self.enemiesArray.removeAll(keepCapacity: false)
+    }
+    
     // Teleport g/s
     func setNumberOfTeleports(teleports : Int) {
         if (teleports <= 0) {
@@ -94,7 +107,22 @@ class Table {
     
     //
     func setFieldOnXY(#x: Int, y: Int, fieldType: Int) {
-        self.contentArray[x][y].setFieldType(fieldType: fieldType)
+        if (fieldType == 0) {
+            self.contentArray[x][y].setFieldType(fieldType: fieldType)
+        }
+        else if (fieldType == 1) {
+            self.contentArray[x][y].setFieldType(fieldType: fieldType)
+            self.player.x = x
+            self.player.y = y
+        }
+        else if (fieldType == 2) {
+            self.contentArray[x][y].setFieldType(fieldType: fieldType)
+            self.contentArray[x][y].setFieldXY(x: x, y: y)
+            self.enemiesArray.append(self.getFieldOnXY(x: x, y: y))
+        }
+        else if (fieldType == 3) {
+            self.contentArray[x][y].setFieldType(fieldType: fieldType)
+        }
     }
     
     // Randomization
@@ -137,7 +165,98 @@ class Table {
     }
     
     // Move methods
+    // in cardinal directions
     
+    // N,E,W,S,NE,NS,WE,WS
+    // N - 1, E - 2, W - 3, S - 4
+    // NE - 5, NS - 6, WE - 7, WS - 8
+    // Return:
+    // -1 if bad values or cant move
+    // 0 if moved
+    // 1 if enemies dead
+    // 2 if player dead
+    
+    func moveN(#x: Int, y: Int, direction: Int) -> Int {
+        if (x < 0 || y < 0 || direction > 8 || direction <= 0){
+            return -1
+        }
+        
+        var xmove = 0, ymove = 0
+        
+        switch(direction) {
+        case 1:
+            if x == 0 {
+                return -1
+            }
+            xmove = -1
+            ymove = 0
+        case 2:
+            if y == self.y {
+                return -1
+            }
+            xmove = 0
+            ymove = 1
+        case 3:
+            if x == self.x {
+                return -1
+            }
+            xmove = 1
+            ymove = 0
+        case 4:
+            if y == 0 {
+                return -1
+            }
+            xmove = 0
+            ymove = -1
+        case 5:
+            if x == 0 || y == self.y {
+                return -1
+            }
+            xmove = -1
+            ymove = 1
+        case 6:
+            if x == 0 || y == 0 {
+                return -1
+            }
+            xmove = -1
+            ymove = -1
+        case 7:
+            if x == self.x || y == self.y {
+                return -1
+            }
+            xmove = 1
+            ymove = 1
+        case 8:
+            if x == self.x || y == 0 {
+                return -1
+            }
+            xmove = 1
+            ymove = -1
+        default:
+            xmove = 0
+            ymove = 0
+        }
+        
+        let fieldType = getFieldOnXY(x: x, y: y).getFieldType()
+
+        // Colisions
+        // If next field is empty, move
+        if (self.getFieldOnXY(x: x+xmove, y: y+ymove).fieldType == 0) {
+            self.setFieldOnXY(x: x, y: y, fieldType: 0)
+            self.setFieldOnXY(x: x+xmove, y: y+ymove, fieldType: fieldType)
+            return 0
+        }
+        // If on next field is player, and enemies invade
+        else if ((self.getFieldOnXY(x: x+xmove, y: y+ymove).fieldType == 1) && fieldType == 2) {
+            //
+            return 2
+        }
+        // If on next field is enemies, and player invade
+        else if ((self.getFieldOnXY(x: x+xmove, y: y+ymove).fieldType == 2) && fieldType == 1) {
+            return 2
+        }
+        return 5
+    }
 }
 
 
